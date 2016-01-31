b6b4eade6de61f5fa0f1e21d01cb6cbd416bfbb8
bet-dice <bet-dice@malwareonlinevir.com>
2016-01-31 14:45:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LeA8RYTAAAAAMTTFJuuhChx6w0A-vp7Yl_Jkrp0',  // <----- EDIT ME!
-  redirect_uri: 'https://bet-dice.github.io',
+  redirect_uri: 'https://bet-dice.github.io/bet-dice/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
